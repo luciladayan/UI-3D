c5970c25abc6a867fc751e409792aeb4b39ac2f5
luciladayan <163851662+luciladayan@users.noreply.github.com>
2024-05-27 09:13:00 -0300

    Ingreso contraseña

---
+++ Assets/IngresoScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngresoScript : MonoBehaviour
+
+{
+    public InputField Input_IngresoPass;
+    public string password = "manzana";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void Checkpassword()
+
+{
+    if (Input_IngresoPass.text == password)
+    { Debug.Log ("Access granted");
+        }
+
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+}
+}
